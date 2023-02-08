75293cdd2635f11cf8a3e560125844a5b16d1685
Dario22-eng <124134212+Dario22-eng@users.noreply.github.com>
2023-02-08 09:15:10 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    TEMPERATURA_EXTERIOR = receivedNumber
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
+let TEMPERATURA_EXTERIOR = 0
+radio.setGroup(2)
